d612e879eb0a5c6c5a87029c2a14304c7b678fc0
mehmetalperen <mhmtalperennadi@gmail.com>
2022-04-13 16:05:34 -0700

    initilize firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>fart</div>;
+  return (
+    <div>
+      Welcome to the Tic-Tac-Toe battle field! You can invite anybody with an
+      internet access and show them you're a Tic-Tac-Toe beast!
+    </div>
+  );
 };
 
 export default Home;
