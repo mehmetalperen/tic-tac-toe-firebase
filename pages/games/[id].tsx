a3fa8a21e09c79ec1fce555e14db7328e8ce3dde
mehmetalperen <mhmtalperennadi@gmail.com>
2022-04-14 16:40:09 -0700

    implement game logic and finish pj

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -17,39 +17,118 @@ interface IGame {
   board: [IRow, IRow, IRow];
 }
 
+const checkWinner = (game: [IRow, IRow, IRow]): boolean => {
+  const checkRow0 =
+    game[0][0] !== "" && game[0][0] === game[0][1] && game[0][1] === game[0][2];
+  const checkRow1 =
+    game[1][0] !== "" && game[1][0] === game[1][1] && game[1][1] === game[1][2];
+  const checkRow2 =
+    game[2][0] !== "" && game[2][0] === game[2][1] && game[2][1] === game[2][2];
+
+  const checkCol0 =
+    game[0][0] !== "" && game[0][0] === game[1][0] && game[1][0] === game[2][0];
+  const checkCol1 =
+    game[0][1] !== "" && game[0][1] === game[1][1] && game[1][1] === game[2][1];
+  const checkCol2 =
+    game[0][2] !== "" && game[0][2] === game[1][2] && game[1][2] === game[2][2];
+
+  const checkCross1 =
+    game[0][0] !== "" && game[0][0] === game[1][1] && game[1][1] === game[2][2];
+  const checkCross2 =
+    game[0][2] !== "" && game[0][2] === game[1][1] && game[1][1] === game[2][0];
+
+  return (
+    checkRow0 ||
+    checkRow1 ||
+    checkRow2 ||
+    checkCol0 ||
+    checkCol1 ||
+    checkCol2 ||
+    checkCross1 ||
+    checkCross2
+  );
+};
+
 export default function Game(id: IProps) {
   const [game, setGame] = useState<IGame | null>(null);
+  const winner = !!game && checkWinner(game.board);
+  const [blockUserMove, setBlockUserMove] = useState(true);
 
   useEffect(() => {
-    console.log(id.id);
     const ref = database.ref(`games/${id.id}`);
     ref.on("value", (snapshot) => {
       //.on() -> listening for changes
       setGame(snapshot.val());
+      setBlockUserMove(false);
     });
 
     return () => ref.off(); //.off() => stop listening for changes
   }, [id]);
+
+  useEffect(() => {
+    console.log(`user can play: ${blockUserMove}`);
+  }, [blockUserMove]);
+
+  const saveMove = (rowIndex: number, colIndex: number) => {
+    if (!game) return;
+    const gameCopy = { ...game };
+    gameCopy.board[rowIndex][colIndex] = game.turn === game.first ? "X" : "O";
+    gameCopy.turn = gameCopy.turn === "player1" ? "player2" : "player1";
+    database.ref(`games/${id.id}`).set(gameCopy);
+
+    setBlockUserMove(true);
+  };
 
   if (!game) {
     return <div>Loading...</div>;
   }
   return (
     <main>
-      <h1>{game.turn === "player1" ? game.player1 : game.player2}'s Turn</h1>
+      {winner ? (
+        <h1>
+          Winner's {game.turn === "player1" ? game.player2 : game.player1}
+        </h1>
+      ) : (
+        <h1>{game.turn === "player1" ? game.player1 : game.player2}'s Turn</h1>
+      )}
       <div className="board">
         {game.board.map((row, rowIndex) => {
           return (
             <div className="row" key={`row-${rowIndex}`}>
               {row.map((col, colIndex) => {
                 return (
-                  <div key={`${rowIndex}-${colIndex}`} className="square"></div>
+                  <div
+                    key={`${rowIndex}-${colIndex}`}
+                    className="square"
+                    onClick={() => {
+                      if (col === "" && !winner && !blockUserMove) {
+                        saveMove(rowIndex, colIndex);
+                      }
+                    }}
+                  >
+                    {col}
+                  </div>
                 );
               })}
             </div>
           );
         })}
       </div>
+      <button
+        onClick={() => {
+          const gameCopy = { ...game };
+          gameCopy.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+          ];
+          gameCopy.first = gameCopy.first === "player1" ? "player2" : "player1";
+          gameCopy.turn = gameCopy.first;
+          database.ref(`games/${id.id}`).set(gameCopy);
+        }}
+      >
+        Reset
+      </button>
     </main>
   );
 }
